bf4cbdd7641898773db66bda5903d35f68afb2f3
Chalermchon Samana <tastsuya_desktop@hotmail.com>
2020-04-16 16:40:41 +0700

    [Update] officer-types get data

--- pages/officertypes/index.js
+++ pages/officertypes/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Layout from '../../components/layouts/Layout'
 import ContentHeader from '../../components/layouts/ContentHeader'
 import ContentBody from '../../components/layouts/ContentBody'
+import ListOfficerTypes from '../../components/officertypes/ListOfficerTypes'
 
 const OfficerTypes = () => {
 
@@ -10,6 +11,9 @@ const OfficerTypes = () => {
             <ContentHeader title={'รายการประเภทบุคคลากรในหน่วยงาน'} />
             <ContentBody>
                 <div>Body Page1</div>
+                <div>
+                    <ListOfficerTypes />
+                </div>
             </ContentBody>
         </Layout>
     )
