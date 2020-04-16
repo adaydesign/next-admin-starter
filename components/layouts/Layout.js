e2bbe3d6c0afe36f3b36efafef316cc317b9a2da
Praphansak <praphansak.p@coj.go.th>
2020-04-16 12:39:41 +0700

    [Create] make new  login page

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -1,4 +1,8 @@
 import React from 'react'
+
+import { useDispatch } from 'react-redux';
+import { clearUserData } from '../../includes/lib/store';
+
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
 import HeaderAppBar from './Header'
@@ -8,7 +12,17 @@ import useStyles from './style'
 
 const Layout = (props) => {
     const classes = useStyles()
+   
+    // redux dispatch
+   const dispatch = useDispatch();
 
+   const logoutHandle = () => {
+       // clear user data
+       dispatch(clearUserData)
+       // redirect
+       Router.push('/')
+   }
+   
     return (
         <div className={classes.root}>
             <CssBaseline />
