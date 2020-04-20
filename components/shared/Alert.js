03caf93ca5ea445ccaab77cbd8df4ff402940c69
Chalermchon Samana <tastsuya_desktop@hotmail.com>
2020-04-20 16:38:53 +0700

    [Edit] delete division -  test

--- components/shared/Alert.js
+++ components/shared/Alert.js
@@ -2,8 +2,10 @@ import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
 const MySwal = withReactContent(Swal)
 
-export default function Alert(props) {
+export default function Alert(props,thenHandle) {
     MySwal.fire({
         ...props
+    }).then(result=>{
+        thenHandle(result)
     })
 }
